bd2a64e9b92c5581f2d9b6a6d5b69c628075d4da
halymat <asekhe2021@gmail.com>
2023-03-28 14:03:45 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const myName = "Sadiat Abu";
+const ProgrammingLanguage = "JavaScript";
+const nextOneYear =
+  "I see my self working with Turing Company as a backend develomper";
+console.log(
+  myName +
+    " Preffered programming language is " +
+    ProgrammingLanguage +
+    "." +
+    " And in the next one year " +
+    nextOneYear
+);
